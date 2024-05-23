ec94ad796960fc728ffec15360720120091c6bde
LeeMo <leemo.tseng@gmail.com>
2024-05-23 09:13:04 +0800

    Configure deployment to GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,4 @@
-import { fileURLToPath, URL } from 'node:url'
+// import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
@@ -6,9 +6,10 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
+  base: '/shopping-web/'
+  // resolve: {
+  // alias: {
+  //   '@': fileURLToPath(new URL('./src', import.meta.url))
+  // }
+  // }
 })
